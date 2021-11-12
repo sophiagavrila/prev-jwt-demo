c69166ceede61c8660ac5244bc2a3e703b5b5698
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-11-12 08:41:56 -0500

    implement loadUserByUsername() method within UserDetailsServiceImpl

---
+++ jwt-backend/src/main/java/com/revature/service/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.revature.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.revature.data.UserRepository;
+import com.revature.model.CustomUserBean;
+import com.revature.model.User;
+
+/**
+ * Within Spring security there is an interface
+ * org.springframework.security.core.userdetails.UserDetailsService that has a
+ * method loadUserByUsername(java.lang.String username) to load user-specific
+ * data.
+ * 
+ * We’ll provide a concrete implementation of this interface where the
+ * loadUserByUsername() method implementation acts as a wrapper over the
+ * userRepository.findByUserName() method call.
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUserName(username)
+				.orElseThrow(() -> new UsernameNotFoundException("User with " + "user name " + username + " not found"));
+
+		/**
+		 * The returned user is passed to the CustomUserBean.createInstance() 
+		 * method to create instance of CustomUserBean as per our implementation.
+		 */
+		return CustomUserBean.createInstance(user);
+	}
+
+}
