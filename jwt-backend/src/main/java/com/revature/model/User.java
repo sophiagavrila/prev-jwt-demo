2b18360ad7ae160cf5ae92b1a7d1d27434e932bc
Sophia Gavrila <57912010+sophiagavrila@users.noreply.github.com>
2021-11-12 07:48:49 -0500

    add User model

---
+++ jwt-backend/src/main/java/com/revature/model/User.java
@@ -0,0 +1,54 @@
+package com.revature.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.management.relation.Role;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "name")
+	private String userName;
+
+	@Column
+	private String email;
+
+	@Column(name = "password")
+	private String password;
+
+	/**
+	 * User has Many-to-Many relationship with Role, that association is captured
+	 * using the join table user_role. 
+	 * 
+	 * LAZY loading will load the User's Roles on-demand when we call the getRoles() 
+	 * method.
+	 * EAGER loading would have loaded it together with the rest of the fields 
+	 * whenever we return a User model.
+	 * 
+	 * https://stackoverflow.com/questions/2990799/difference-between-fetchtype-lazy-and-eager-in-java-persistence-api
+	 */
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "USER_ID", referencedColumnName = "ID"), inverseJoinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID"))
+
+	private Set<Role> roles = new HashSet<>();
+
+}
